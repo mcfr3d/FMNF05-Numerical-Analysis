5dbc9d8ff9ac7ab6d1531d912c9da6e7b20598c6
Fredrik <nyberg_11@hotmail.com>
2018-02-19 17:36:15 +0100

    added points manually chosen

---
+++ projekt 2/getKnotsManually.m
@@ -0,0 +1,19 @@
+%I = imread('redCar.png');
+%I=imrotate(I,180);
+%I = flip(I,2);
+%imshow(I);
+
+A = [cursor_info(1).Position];
+X=[A(1)];
+Y=[A(2)];
+
+for i =2:21
+    B = cursor_info(i).Position;
+    X =[X B(1)];
+    Y =[Y  B(2)];
+    end
+    
+    
+%[x,y] = A;
+plot(X,Y,'*')
+my_test();
