5326da6a33159d994a381fad866d0fbf4345f4b8
Fredrik Nyberg <htx10fny@student.lu.se>
2018-02-19 12:59:50 +0100

    tested

--- projekt 2/test.m
+++ projekt 2/test.m
@@ -1,1 +1,13 @@
-imshow('redCar.png')
+I = imread('redCar.png');
+I=imrotate(I,180);
+I = flip(I,2);
+imshow(I)
+
+
+A = [cursor_info(1).Position];
+for i =2:21
+    B = cursor_info(i).Position;
+    A =[A;B(1) B(2)]; 
+end
+%[x,y] = A;
+plot(A,'*')
