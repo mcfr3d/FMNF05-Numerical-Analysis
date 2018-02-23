e4237120408c6fa22cc20bac6274bcfafa9efaa0
axel-syren <dat15asy@student.lu.se>
2018-02-23 17:58:47 +0100

    Det funkar!

--- projekt 2/task4.m
+++ projekt 2/task4.m
@@ -1,30 +1,43 @@
 tau = 1;
-n = 6;
+n = 10;
 syms f(x);
 f(x) = cos(x);
-x = linspace(0, pi, n+1);
+x = linspace(0, pi, n);
 y = f(x);
-
+subplot(2,1,1);
+plot(x,y);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1 1])
 
 
 h(1:n-1) = x(2:n) - x(1:n-1);
 [alpha, beta, gamma] = getAlphaBetaGamma(tau,h,y, n);
 A = getA(alpha, beta, n)
-r = getR(gamma, n, -1, 1)
-z =A\r        % solve for z coefficients
-
+cond1 = 0;
+cond2 = 0;
+r = getR(gamma, n, cond1, cond2)
+z =A\r        % solve for z
 
 
 %T(x) = (z(i)*sinh(tau*(x(i+1)-x)) +
 %z(i+1)*sinh(tau*(x-x(i))))/(tau^2*sinh(tau*h(i))) + 
 %((y(i)-z(i)/(tau^2))(x(i+1)-x)+(y(i+1)-z(i+1)/(tau^2))(x-x(i)))/h(i)
 
-T = zeros(1,n-1);
 
+T = @(i,xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
+xs = zeros(10,n-1);
+subplot(2,1,2)
+hold on
+plot(x,y,'.')
 for i = 1:n-1
-    syms Ts(xs);
-    Ts(xs) = (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
-    T(i) = @Ts;
+    xs(:,i) = linspace(x(i),x(i+1),10);
+    plot(xs(:,i),T(i,xs(:,i)))
 end
 
-T
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1 1])
+
+%T(i) = @(xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
+
