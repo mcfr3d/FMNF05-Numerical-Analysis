ddd92abb3d6aaf2c97847ff482f63990bbd104e8
axel-syren <dat15asy@student.lu.se>
2018-02-25 14:01:59 +0100

    ...

--- projekt 2/task4.m
+++ projekt 2/task4.m
@@ -5,30 +5,34 @@
 % y = f(x);
 
 I = imread('redCar.png');
-[x,y] = getPredefinedKnots('pointData/cursorinfo3.mat')
-n = length(x)
-tau = 10;
 
-h(1:n-1) = x(2:n) - x(1:n-1);
-[alpha, beta, gamma] = getAlphaBetaGamma(tau,h,y, n);
-A = getA(alpha, beta, n);
+[x1,y1] = getPredefinedKnots('pointData/cursorinfo4.mat');
+tau = 0.4;
+%cond1 = 0.02*tau;
+%cond2 = -0.09*tau;
+cond1 = 1/5;
+cond2 = -0.006;
+T1 = tensionSplines(x1, y1, tau, cond1, cond2);
+[x2,y2] = getPredefinedKnots('pointData/undercarriage.mat');
+tau = 0.2;
 cond1 = 0.02*tau;
 cond2 = -0.09*tau;
-r = getR(gamma, n, cond1, cond2);
-z =A\r;        % solve for z
-
-
-T = @(i,xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
+T2 = tensionSplines(x2, y2, tau, cond1, cond2);
 
 
 imshow(I);
 hold on
-% plot(x,ones(n,1)*258 - y,'.')
+% plot(x,ones(ns,1)*258 - y,'.')
 % plot(x,y,'.')
-for i = 1:n-1
-%     fplot(@(t) 258-T(i,t), [x(i) x(i+1)], 'b');
-    fplot(@(t) T(i,t), [x(i) x(i+1)], 'b');
+for i = 1:length(x1)-1
+%     fplot(@(t) 258-T1(i,t), [x1(i) x1(i+1)], 'b');
+    fplot(@(t) T1(i,t), [x1(i) x1(i+1)], 'b');
 end
+for i = 1:length(x2)-1
+%     fplot(@(t) 258-T2(i,t), [x2(i) x2(i+1)], 'b');
+    fplot(@(t) T2(i,t), [x2(i) x2(i+1)], 'g');
+end
+
 
 daspect([1 1 1])
 xlim([0 763])
