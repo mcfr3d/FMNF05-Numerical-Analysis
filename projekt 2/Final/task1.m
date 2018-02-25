11f97930de8080235561d53d05ece43022617e2c
axel-syren <dat15asy@student.lu.se>
2018-02-25 17:06:02 +0100

    Fixed task1.m

--- projekt 2/Final/task1.m
+++ projekt 2/Final/task1.m
@@ -3,7 +3,7 @@ I = imread('redCar.png');
 imshow(I);
 hold on
 title('Task 1');
-[x,y] = getKnots(I,40);
+[x,y] = getPredefinedKnots('../pointData/roof.mat');
 xq = [min(x):max(x)];
 s = spline(x,y,xq);
-plot(x,ones(length(y),1)*258 - y,'o', xq, ones(1,length(s))*258 - s);
+plot(x,y,'o', xq, s);
