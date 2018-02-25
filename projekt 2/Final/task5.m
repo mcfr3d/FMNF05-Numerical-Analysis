d324ec372c85550cba18241159648a93ac06498c
axel-syren <dat15asy@student.lu.se>
2018-02-25 17:00:15 +0100

    Fied task5.m

--- projekt 2/Final/task5.m
+++ projekt 2/Final/task5.m
@@ -11,13 +11,13 @@ xlim([0 pi])
 ylim([-1 1])
 subplot(3,2,2);
 tensionsplineplot(x,y,0.3);
-title('tau = 1');
+title('tau = 0.3');
 daspect([1 1 1])
 xlim([0 pi])
 ylim([-1 1])
 subplot(3,2,3);
 tensionsplineplot(x,y,1);
-title('tau = 2');
+title('tau = 1');
 daspect([1 1 1])
 xlim([0 pi])
 ylim([-1 1])
@@ -29,7 +29,7 @@ xlim([0 pi])
 ylim([-1 1])
 subplot(3,2,5);
 tensionsplineplot(x,y,10);
-title('tau = 4');
+title('tau = 10');
 daspect([1 1 1])
 xlim([0 pi])
 ylim([-1 1])
--- projekt 2/task4.m
+++ projekt 2/task4.m
@@ -1,32 +1,48 @@
 I = imread('redCar.png');
 
-[x1,y1] = getPredefinedKnots('pointData/roof.mat');
-tau = 0.2;
-cond1 = 1/5;
-cond2 = -1/2;
-T1 = tensionSplines(x1, y1, tau, cond1, cond2);
-[x2,y2] = getPredefinedKnots('pointData/undercarriage.mat');
-tau = 0.4;
-cond1 = -1;
-cond2 = 1;
-T2 = tensionSplines(x2, y2, tau, cond1, cond2);
-[x3,y3] = getPredefinedKnots('pointData/window_top.mat');
-tau = 0.2;
-cond1 = 1;
-cond2 = -1;
-T3 = tensionSplines(x3, y3, tau, cond1, cond2);
-[x4,y4] = getPredefinedKnots('pointData/window_bottom.mat');
-tau = 0.2;
-cond1 = -1;
+n = 10;
+x = linspace(0, pi, n)
+y = cos(x)
+
+tau = 10;
+cond1 = 0;
 cond2 = 0;
-T4 = tensionSplines(x4, y4, tau, cond1, cond2);
+T = tensionSplines(x, y, tau, cond1, cond2);
+hold on
+for i = 1:n-1
+    fplot(@(t) T(i,t), [x(i) x(i+1)], 'r');
+end
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1 1])
+
+% [x1,y1] = getPredefinedKnots('pointData/roof.mat');
+% tau = 0.2;
+% cond1 = 1/5;
+% cond2 = -1/2;
+% T1 = tensionSplines(x1, y1, tau, cond1, cond2);
+% [x2,y2] = getPredefinedKnots('pointData/undercarriage.mat');
+% tau = 0.4;
+% cond1 = -1;
+% cond2 = 1;
+% T2 = tensionSplines(x2, y2, tau, cond1, cond2);
+% [x3,y3] = getPredefinedKnots('pointData/window_top.mat');
+% tau = 0.2;
+% cond1 = 1;
+% cond2 = -1;
+% T3 = tensionSplines(x3, y3, tau, cond1, cond2);
+% [x4,y4] = getPredefinedKnots('pointData/window_bottom.mat');
+% tau = 0.2;
+% cond1 = -1;
+% cond2 = 0;
+% T4 = tensionSplines(x4, y4, tau, cond1, cond2);
 
 
 % imshow(I);
-hold on
-for i = 1:length(x1)-1
-    fplot(@(t) T1(i,t), [x1(i) x1(i+1)], 'b');
-end
+% hold on
+% for i = 1:length(x1)-1
+%     fplot(@(t) T1(i,t), [x1(i) x1(i+1)], 'b');
+% end
 % for i = 1:length(x2)-1
 %     fplot(@(t) T2(i,t), [x2(i) x2(i+1)], 'b');
 % end
@@ -38,9 +54,9 @@ end
 % end
 
 
-daspect([1 1 1])
-xlim([0 763])
-ylim([0 258])
+% daspect([1 1 1])
+% xlim([0 763])
+% ylim([0 258])
 
 %T(i) = @(xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
 
