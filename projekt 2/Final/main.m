98a7db076079a7e2c50da03e2a8acbd1e98f5e64
axel-syren <dat15asy@student.lu.se>
2018-02-25 15:03:19 +0100

    Task5

--- projekt 2/Final/main.m
+++ projekt 2/Final/main.m
@@ -6,6 +6,40 @@
 %tau=1;
 %tensionsplineplot(x,y,tau);
 
+
+%Task 5
+figure
+n = 10;
+x = linspace(0, pi, n);
+y = cos(x);
+subplot(3,2,1);
+tensionsplineplot(x,y,0);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1.2 1.2])
+subplot(3,2,2);
+tensionsplineplot(x,y,0.3);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1.2 1.2])
+subplot(3,2,3);
+tensionsplineplot(x,y,1);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1.2 1.2])
+subplot(3,2,4);
+tensionsplineplot(x,y,3);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1.2 1.2])
+subplot(3,2,5);
+tensionsplineplot(x,y,10);
+daspect([1 1 1])
+xlim([0 pi])
+ylim([-1.2 1.2])
+
+
+figure
 I = imread('redCar.png');
 [x,y] = getPredefinedKnots('pointData/cursorinfo3.mat');
 tau=0.1;
