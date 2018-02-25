ca63dc0743f73a15d5dbb3b46ad558fa176853a9
axel-syren <dat15asy@student.lu.se>
2018-02-25 13:38:09 +0100

    Fixade task4.m

--- projekt 2/task4.m
+++ projekt 2/task4.m
@@ -1,43 +1,38 @@
-tau = 1;
-n = 10;
-syms f(x);
-f(x) = cos(x);
-x = linspace(0, pi, n);
-y = f(x);
-subplot(2,1,1);
-plot(x,y);
-daspect([1 1 1])
-xlim([0 pi])
-ylim([-1 1])
+% n = 10;
+% syms f(x);
+% f(x) = x+2;
+% x = linspace(0, pi, n);
+% y = f(x);
 
+I = imread('redCar.png');
+[x,y] = getPredefinedKnots('pointData/cursorinfo3.mat')
+n = length(x)
+tau = 10;
 
 h(1:n-1) = x(2:n) - x(1:n-1);
 [alpha, beta, gamma] = getAlphaBetaGamma(tau,h,y, n);
-A = getA(alpha, beta, n)
-cond1 = 0;
-cond2 = 0;
-r = getR(gamma, n, cond1, cond2)
-z =A\r        % solve for z
-
-
-%T(x) = (z(i)*sinh(tau*(x(i+1)-x)) +
-%z(i+1)*sinh(tau*(x-x(i))))/(tau^2*sinh(tau*h(i))) + 
-%((y(i)-z(i)/(tau^2))(x(i+1)-x)+(y(i+1)-z(i+1)/(tau^2))(x-x(i)))/h(i)
+A = getA(alpha, beta, n);
+cond1 = 0.02*tau;
+cond2 = -0.09*tau;
+r = getR(gamma, n, cond1, cond2);
+z =A\r;        % solve for z
 
 
 T = @(i,xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
-xs = zeros(10,n-1);
-subplot(2,1,2)
+
+
+imshow(I);
 hold on
-plot(x,y,'.')
+% plot(x,ones(n,1)*258 - y,'.')
+% plot(x,y,'.')
 for i = 1:n-1
-    xs(:,i) = linspace(x(i),x(i+1),10);
-    plot(xs(:,i),T(i,xs(:,i)))
+%     fplot(@(t) 258-T(i,t), [x(i) x(i+1)], 'b');
+    fplot(@(t) T(i,t), [x(i) x(i+1)], 'b');
 end
 
 daspect([1 1 1])
-xlim([0 pi])
-ylim([-1 1])
+xlim([0 763])
+ylim([0 258])
 
 %T(i) = @(xs) (z(i)*sinh(tau*(x(i+1)-xs)) + z(i+1)*sinh(tau*(xs-x(i))))/(tau^2*sinh(tau*h(i))) + ((y(i)-z(i)/(tau^2))*(x(i+1)-xs)+(y(i+1)-z(i+1)/(tau^2))*(xs-x(i)))/h(i);
 
